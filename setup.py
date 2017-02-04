7c0e352574e10b097a2d39763633b21efdd16ce1
Derek Pryor <Derek.Pryor@jhuapl.edu>
2017-02-03 16:35:32 -0800

    Initial commit of setup.py
    (cherry picked from commit 7f32eace192738a39d5f581981bfac4705841cbc)

---
+++ setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+# to update
+# python setup.py sdist
+# python setup.py bdist_wheel
+# twine upload dist/*
+
+
+__version__ = '0.9.2'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='boss-ingest',
+    version=__version__,
+
+    description='Ingest client for the BOSS',
+    long_description=long_description,
+
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    
+    author='Johns Hopkins University Applied Physics Laboratory',
+    author_email='iarpamicrons@jhuapl.edu',
+
+    scripts=['boss-ingest'],
+    packages=['bossingest'],
+    include_package_data=True,
+
+    url='https://github.com/jhuapl-boss/ingest-client',
+    #download_url='https://github.com/jhuapl-boss/ingest-client/tarball/' + __version__,
+
+    license='Apache 2.0',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Programming Language :: Python :: 3.5',
+    ],
+    keywords=[
+        'brain',
+        'microscopy',
+        'neuroscience',
+        'connectome',
+        'connectomics',
+        'spatial',
+        'EM',
+        'electron',
+        'calcium',
+        'database',
+        'boss',
+        'microns'
+    ]
+)
